ee2842f9094507d15ae4d929cb70f60d29cb5b02
waoler <waoler@users.noreply.github.com>
2019-11-19 00:17:46 +0100

    Update unit.js

--- test/unit.js
+++ test/unit.js
@@ -2,4 +2,11 @@ const path = require("path");
 const { tests } = require("@iobroker/testing");
 
 // Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.unit(path.join(__dirname, ".."));
+tests.unit(path.join(__dirname, ".."), {
+    //     ~~~~~~~~~~~~~~~~~~~~~~~~~
+    // This should be the adapter's root directory
+
+    // If the adapter may call process.exit during startup, define here which exit codes are allowed.
+    // By default, no exit codes are allowed.
+    allowedExitCodes: [11]
+});
