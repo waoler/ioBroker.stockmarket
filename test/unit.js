9e229e9ea10fe0a092e4a9da5fd0c7888d3fa0e6
waoler <waoler@users.noreply.github.com>
2019-11-19 00:24:19 +0100

    intefration.js missing exitcodes

--- test/unit.js
+++ test/unit.js
@@ -3,10 +3,5 @@ const { tests } = require("@iobroker/testing");
 
 // Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
 tests.unit(path.join(__dirname, ".."), {
-    //     ~~~~~~~~~~~~~~~~~~~~~~~~~
-    // This should be the adapter's root directory
-
-    // If the adapter may call process.exit during startup, define here which exit codes are allowed.
-    // By default, no exit codes are allowed.
     allowedExitCodes: [11]
 });
