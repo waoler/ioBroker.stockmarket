5a1a24cf0bf3d4ab69dc65fd79e5ca851bcf0acb
Thorsten Reichelt <info@thorstenreichelt.de>
2021-08-02 14:14:12 +0200

    added translations

--- admin/words.js
+++ admin/words.js
@@ -18,25 +18,73 @@ systemDictionary = {
 	"ownStocks": {
 		"en": "Your favorite stocks",
 		"de": "Deine gewünschten Aktien",
+		"ru": "Ваши любимые акции",
+	        "pt": "Suas ações favoritas",
+	        "nl": "Uw favoriete aandelen",
+	        "fr": "Vos actions préférées",
+	        "it": "Le tue azioni preferite",
+	        "es": "Tus acciones favoritas",
+	    	"pl": "Twoje ulubione akcje",
+	    	"zh-cn": "你最喜欢的股票"
 	},
 	"ownStocks_label": {
 		"en": "Stockids (comma separated)",
 		"de": "Aktiennummer (mit Komma trennen)",
+		"ru": "Stockids (через запятую)",
+    		"pt": "Stockids (separados por vírgula)",
+    		"nl": "Stockids (komma gescheiden)",
+    		"fr": "Stockids (séparés par des virgules)",
+    		"it": "Stockids (separati da virgole)",
+    		"es": "Stockids (separados por comas)",
+    		"pl": "Stockids (oddzielone przecinkami)",
+    		"zh-cn": "Stockids（逗号分隔）"
 	},
 	"ownStocks_info": {
 		"en": "Enter your stock ids here. No spaces please. Separate multiple values with comma.<br> You can find your stock ids with this URL:<br><a href=\"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY\">https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY</a>",
 		"de": "Hier die Aktiennummer eintragen. Bitte ohne Leerzeichen. Mehrere Werte mit Komma trennen.<br> Einge Suche der Aktiennummer ist über die folgende URL möglich:<br><a href=\"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY\">https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY</a>",
+		"ru": "Введите сюда идентификаторы ваших акций. Без пробелов, пожалуйста. Несколько значений разделяйте запятыми.<br> Вы можете найти идентификаторы своих акций по этому URL-адресу:<br> <a href=\\\"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY\\\">https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY</a>",
+    		"pt": "Insira seus IDs de estoque aqui. Sem espaços, por favor. Separe vários valores com vírgulas.<br> Você pode encontrar seus IDs de estoque com este URL:<br> <a href=\\\"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY\\\">https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY</a>",
+    		"nl": "Voer hier uw voorraad-ID's in. Geen spaties alstublieft. Scheid meerdere waarden met een komma.<br> U kunt uw aandelen-ID's vinden met deze URL:<br> <a href=\\\"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY\\\">https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY</a>",
+    		"fr": "Entrez vos identifiants de stock ici. Pas d'espace, s'il vous plait. Séparez plusieurs valeurs par une virgule.<br> Vous pouvez trouver vos identifiants de stock avec cette URL :<br> <a href=\\\"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY\\\">https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY</a>",
+    		"it": "Inserisci qui i tuoi ID stock. Niente spazi, per favore. Separare più valori con la virgola.<br> Puoi trovare i tuoi ID stock con questo URL:<br> <a href=\\\"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY\\\">https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY</a>",
+    		"es": "Ingrese sus ID de acciones aquí. Sin espacios, por favor. Separe varios valores con comas.<br> Puede encontrar sus ID de acciones con esta URL:<br> <a href=\\\"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY\\\">https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY</a>",
+    		"pl": "Wpisz tutaj swoje identyfikatory giełdowe. Bez spacji, prosze. Poszczególne wartości oddziel przecinkami.<br> Swoje identyfikatory giełdowe można znaleźć pod tym adresem URL:<br> <a href=\\\"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY\\\">https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=NAZWA AKCJI&apikey=TWÓJ_KLUCZ_API</a>",
+    		"zh-cn": "在此处输入您的股票编号。请不要空格。用逗号分隔多个值。<br>您可以通过以下 URL 找到您的股票 ID：<br> <a href=\\\"https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY\\\">https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=STOCKNAME&apikey=YOUR_API_KEY</a>"
 	},
 	"apikey": {
 		"en": "Your API Key",
 		"de": "Dein API Key",
+		"ru": "Ваш ключ API",
+    		"pt": "Sua chave de API",
+    		"nl": "Uw API-sleutel",
+    		"fr": "Votre clé API",
+    		"it": "La tua chiave API",
+    		"es": "Tu clave de API",
+    		"pl": "Twój klucz API",
+    		"zh-cn": "您的 API 密钥"
 	},
 	"apikey_label": {
 		"en": "Insert your API Key",
 		"de": "API Key hier eintragen",
+		"ru": "Вставьте свой ключ API",
+    		"pt": "Insira sua chave de API",
+    		"nl": "Voer uw API-sleutel in",
+    		"fr": "Insérez votre clé API",
+    		"it": "Inserisci la tua chiave API",
+    		"es": "Inserta tu clave API",
+    		"pl": "Wstaw swój klucz API",
+    		"zh-cn": "插入您的 API 密钥"
 	},
 	"apikey_info": {
 		"en": "Please check <a href=\"https://www.alphavantage.co/support/#api-key\">https://www.alphavantage.co/support/#api-key</a>, to get your own API Key",
-		"de": "Bitte registriere deinen eigenen API Key unter der folgenden Adresse: <a href=\"https://www.alphavantage.co/support/#api-key\">https://www.alphavantage.co/support/#api-key</a>",
+		"de": "Bitte überprüfen Sie <a href=\\\"https://www.alphavantage.co/support/#api-key\\\">https://www.alphavantage.co/support/#api-key</a> , um Ihren eigenen API-Schlüssel zu erhalten",
+    		"ru": "Пожалуйста, проверьте <a href=\\\"https://www.alphavantage.co/support/#api-key\\\">https://www.alphavantage.co/support/#api-key</a> , чтобы получить собственный ключ API",
+    		"pt": "Verifique <a href=\\\"https://www.alphavantage.co/support/#api-key\\\">https://www.alphavantage.co/support/#api-key</a> , para obter sua própria chave de API",
+    		"nl": "Kijk op <a href=\\\"https://www.alphavantage.co/support/#api-key\\\">https://www.alphavantage.co/support/#api-key</a> , om uw eigen API-sleutel te krijgen",
+    		"fr": "Veuillez vérifier <a href=\\\"https://www.alphavantage.co/support/#api-key\\\">https://www.alphavantage.co/support/#api-key</a> , pour obtenir votre propre clé API",
+    		"it": "Si prega di controllare <a href=\\\"https://www.alphavantage.co/support/#api-key\\\">https://www.alphavantage.co/support/#api-key</a> , per ottenere la propria chiave API",
+    		"es": "Consulte <a href=\\\"https://www.alphavantage.co/support/#api-key\\\">https://www.alphavantage.co/support/#api-key</a> para obtener su propia clave API",
+    		"pl": "Sprawdź <a href=\\\"https://www.alphavantage.co/support/#api-key\\\">https://www.alphavantage.co/support/#api-key</a> , aby uzyskać własny klucz API",
+    		"zh-cn": "请检查<a href=\\\"https://www.alphavantage.co/support/#api-key\\\">https://www.alphavantage.co/support/#api-key</a> ，以获取您自己的 API 密钥"
 	}
 };
